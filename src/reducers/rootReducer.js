e62562f4d9aa0ca44ab483bab87c8af032f256c5
awbridgers <awbridgers@users.noreply.github.com>
2020-01-13 15:39:51 -0500

    update reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { shoppingCart } from './shoppingCart.js'
 import { shippingAddress} from './shippingAddress.js'
-import { shippingCost} from './shppingCost.js'
+import { shippingCost} from './shippingCost.js'
 
 const rootReducer = combineReducers({
   shoppingCart,
--- src/reducers/shippingAddress.js
+++ src/reducers/shippingAddress.js
@@ -1,8 +1,19 @@
 
-export const shippingAddress = (state = {},action)=>{
+export const shippingAddress = (state = {
+  name: 'Test Name',
+  email: 'testemail@test.com',
+  phone: '1234567890',
+  city: 'Test City',
+  state: 'Test State',
+  address: '123 Test St. Apt. 1',
+  zip: '12345'
+},action)=>{
   switch(action.type){
-    case 'CHANGE_SHIPPING_ADDRESS':
-      return action.obj
+    case 'CHANGE_SHIPPING_INFO':
+      return {
+        ...state,
+        [action.key]: action.payload
+      }
     default:
       return state
   }
